ca1b6b237409a49914800ca4e65490a830fdfa39
RamadaLord <petersongeraldo94@gmail.com>
2023-04-10 13:33:04 -0300

    Att

--- MegaEigaHd/megaeigahd/src/Components/Home/Home.js
+++ MegaEigaHd/megaeigahd/src/Components/Home/Home.js
@@ -6,6 +6,7 @@ import ImageH from "../../img/hbo.jpg";
 import ImageN from "../../img/netflix.jpg";
 import ImageT from "../../img/telecine.jpg";
 import ImageG from "../../img/globoplay.jpg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -15,16 +16,28 @@ export default function Home() {
       </div>
       <div className="img">
         <div className="imge1">
-          <img src={ImageP} alt={ImageP} />
-          <img src={ImageD} alt={ImageD} />
+          <Link to="https://www.primevideo.com/">
+            <img src={ImageP} alt={ImageP} />
+          </Link>
+          <Link to="https://www.starplus.com/pt-br/">
+            <img src={ImageD} alt={ImageD} />
+          </Link>
         </div>
         <div className="imge2">
-          <img src={ImageH} alt={ImageH} />
-          <img src={ImageN} alt={ImageN} />
+          <Link to="https://www.hbomax.com/br/pt">
+            <img src={ImageH} alt={ImageH} />
+          </Link>
+          <Link to="https://www.netflix.com/br/">
+            <img src={ImageN} alt={ImageN} />
+          </Link>
         </div>
         <div className="imge3">
-          <img src={ImageT} alt={ImageT} />
-          <img src={ImageG} alt={ImageG} />
+          <Link to="https://www.telecine.com.br">
+            <img src={ImageT} alt={ImageT} />
+          </Link>
+          <Link to="https://globoplay.globo.com">
+            <img src={ImageG} alt={ImageG} />
+          </Link>
         </div>
       </div>
     </div>
