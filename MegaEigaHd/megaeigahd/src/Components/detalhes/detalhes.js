2355210750cfb3362d667691c001a16d11960a76
Peterson Geraldo <petersongeraldo94@gmail.com>
2023-04-18 17:18:26 -0300

    att
    att

--- MegaEigaHd/megaeigahd/src/Components/detalhes/detalhes.js
+++ MegaEigaHd/megaeigahd/src/Components/detalhes/detalhes.js
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom"
 import {useEffect, useState} from 'react'
 import api from "../../services/api";
+import moment from "moment";
 
 export default function Detalhes(){
 
     const {id} = useParams();
     const [filmes , setFilme] = useState({});//chama de objeto//
+    const [filme, setFilmes] = useState([''])
 
     useEffect(() => {
         async function loadFilmeDetails (){
@@ -17,6 +19,7 @@ export default function Detalhes(){
                 }
             })
             setFilme(response.data)
+            setFilmes(response.data.production_companies)
         } 
         console.log(filmes)
         loadFilmeDetails()
@@ -24,20 +27,32 @@ export default function Detalhes(){
 
     return(
         <div className="api-content">
+            
                 <h1 className="des2" >Movie : {filmes.title}
                 <br/>
-                Release Date: {filmes.release_date}
+                Release: {moment(new Date(`${filmes.release_date}`)).format('DD-MM-YYYY')}
                 <br/>
                 Popularity: 
                  {filmes.popularity}
                 <br/>
                 TMDB: ★
-                 {filmes.vote_average}
+                {Number(filmes.vote_average).toFixed(1)}
+                 
                 </h1>
             <div className="api-api" >
+            {filme.map((det) => {
+            return(
+                <div>
+                    <h1>Studio:{det.name}</h1>
+                </div>
+            )
+           })}
                 <img className="img-api"
-                src={`https://image.tmdb.org/t/p/w400/${filmes.poster_path}`}
+                src={`https://image.tmdb.org/t/p/w400/${filmes.backdrop_path}`}
                 alt={filmes.tittle}/>
+                <h3>
+
+                </h3>
             </div>
                 <h2 className="des2">Details : {filmes.overview}</h2>
         </div>
--- MegaEigaHd/megaeigahd/src/pages/Emcartaz/emcartaz.js
+++ MegaEigaHd/megaeigahd/src/pages/Emcartaz/emcartaz.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 export default function Emcartaz() {
                                                      //array de string vazia//
