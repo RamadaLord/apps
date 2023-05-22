e3b4d51fe5286d0de4bcbaa3e7c03ce7325bb9fb
Peterson Geraldo <petersongeraldo94@gmail.com>
2023-05-22 17:18:03 -0300

    att

--- MegaEigaHd/megaeigahd/src/Components/Header/Header.js
+++ MegaEigaHd/megaeigahd/src/Components/Header/Header.js
@@ -1,48 +1,91 @@
-import Image from '../../favicon.png'
-import { Link } from 'react-router-dom'
-import Contatos from '../Contatos/cadastro'
+import Image from "../../favicon.png";
+import { Link } from "react-router-dom";
+import Contatos from "../Contatos/cadastro";
 
 export default function Header() {
-    return (
-        <div>
-             {/* className='container-fluid' */}
-            <div class="cabecalho">
-                <div class="area-logo">
-                    <img src={Image} alt={Image} />
-                </div>
-                <div class="area-texto">
-                    <h1>MegaEigaHD</h1>
-                </div>
-            </div>
-            <div class="menu">
-            <Link className='link' to='/'>Home</Link>
-                </div>
-            <div class="menu">
-                <div className="dropdown">
-                    <a href="#">Filmes</a>
-                    <div className="drop">
-                        <Link className='link' to='/Popularesfilmes'>Populares</Link>
-                        <Link className='link' to='/Emcartaz'>Em cartaz</Link>
-                        <Link className='link' to='/Estreias'>Estreias</Link>
-                    </div>
-                </div>
-                <div>
-                    <Link to="/Favoritos">
-                    
-                    Favoritos
-                    </Link>
-                </div>
-                
-                <div className="dropdown">
-                    <a href="#">Series</a>
-                    <div className="drop">
-                        <Link className='link' to='/Popularesseries'>Populares</Link>
-                        <Link className='link' to='/Exibidos'>Exibir Hoje</Link>
-                        <Link className='link' to='Streamming'>Streamming</Link>
-                    </div>
-                </div>
-                <Contatos />
-            </div>
+  return (
+    <div>
+      {/* className='container-fluid' */}
+      <div class="cabecalho">
+        <div class="area-logo">
+          <img src={Image} alt={Image} />
         </div>
-    )
+        <div class="area-texto">
+          <h1>MegaEigaHD</h1>
+        </div>
+      </div>
+      <div class="menu">
+        <Link
+          className="link"
+          to="/"
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+          }}
+        >
+          Home
+        </Link>
+      </div>
+      <div class="menu">
+        <div className="dropdown">
+          <a href="#">Filmes</a>
+          <div className="drop">
+            <Link className="link"  onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }} to="/Popularesfilmes">
+              Populares
+            </Link>
+            <Link className="link"  onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }} to="/Emcartaz">
+              Em cartaz
+            </Link>
+            <Link className="link"  onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }} to="/Estreias">
+              Estreias
+            </Link>
+          </div>
+        </div>
+        <div>
+          <Link to="/Favoritos"  onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }}>Favoritos</Link>
+        </div>
+
+        <div className="dropdown">
+          <a href="#">Series</a>
+          <div className="drop">
+            <Link
+              className="link"
+              to="/Popularesseries"
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+            >
+              Populares
+            </Link>
+            <Link
+              className="link"
+              to="/Exibidos"
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+            >
+              Exibir Hoje
+            </Link>
+            <Link
+              className="link"
+              to="Streamming"
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+            >
+              Streamming
+            </Link>
+          </div>
+        </div>
+        <Contatos />
+      </div>
+    </div>
+  );
 }
--- MegaEigaHd/megaeigahd/src/Components/footer/footer.js
+++ MegaEigaHd/megaeigahd/src/Components/footer/footer.js
@@ -1,22 +1,27 @@
-import { Link } from 'react-router-dom'
-import '../css/style.footer.css'
-import image from '../../favicon.png'
-export default function Footer(){
-    return(
-        <div className='footer'>
-            <footer>
-                <Link to='/'>
-                <img className='F-img' src={image} alt={image}/>
-                </Link>
-                <div  className='pes'>
-
-                Copyright©
-                <Link to="https://github.com/RamadaLord">
-                :RamadaLord
-                </Link>
-                </div>
-            </footer>
+import { Link } from "react-router-dom";
+import "../css/style.footer.css";
+import image from "../../favicon.png";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <footer>
+        <Link to="/"  onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }}>
+          <img
+            className="F-img"
+            onClick={() => {
+              window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+            }}
+            src={image}
+            alt={image}
+          />
+        </Link>
+        <div className="pes">
+          Copyright©
+          <Link to="https://github.com/RamadaLord">:RamadaLord</Link>
         </div>
-    )
+      </footer>
+    </div>
+  );
 }
-
